1547abfc0776d477dfc62838776ecb36d97ab41b
bogu <boguxdeus@protonmail.com>
2024-04-01 21:10:00 +0200

    Refactor: header-only, use virtual functions for externals definition, new macros

--- BetterExternals.h
+++
@@ -1,7 +0,0 @@
-#include "ExternalUtility.h"
-#include "Externals.h"
-
-//include your files here
-//#include "MyExternals.h"
-
-#include "ExternalsDefinition.h"
--- Externals.h
+++ Externals.h
@@ -2,41 +2,254 @@
 // Union HEADER file
 #include <list>
 #include <concepts>
+#include <list>
+#include <string_view>
+#include <type_traits>
+#include <array>
+#include <ranges>
+#include <vector>
+#include <algorithm>
 
-#define MakeDaedalusExternal(function) Externals::DaedalusExternal<#function,function>
-#define MakeDaedalusExternalWithCondition(function, condition) Externals::DaedalusExternal<#function,function,condition>
+#define BetterDaedalusExternal(function) new Externals::DaedalusExternal<#function,function>{}
+#define BetterDaedalusExternalWithCondition(function, condition) new Externals::DaedalusExternal<#function,function,condition>{}
+
+#define BetterExternalDefinition(parserEnum, ...)\
+inline const Externals::ExternalTable g_externalTable_##parserEnum{ eParser::parserEnum, { __VA_ARGS__ } }; \
+template<> struct Externals::ExternalTableGuard<eParser::parserEnum> {}\
+
 
 namespace GOTHIC_ENGINE
 {
-	enum class eParser
-	{
-		GAME,
-		MENU,
-		MUSIC,
-		SFX,
-		PFX,
-		VFX,
-		MAX
-	};
-
-#define ExternalDefinition(ParserEnum,...) \
-	namespace Externals\
-	{\
-		template<>\
-		struct ExternalTable<ParserEnum>\
-		{\
-			static constexpr const auto s_parser = ParserEnum; \
-			using Table = ExternalsTuple\
-			<\
-			__VA_ARGS__\
-			>; \
-		}; \
-	}\
-
-
-
 	namespace Externals
 	{	
+		enum class eParser
+		{
+			GAME,
+			MENU,
+			MUSIC,
+			SFX,
+			PFX,
+			VFX,
+			MAX
+		};
+
+		constexpr bool MpBuild()
+		{
+#if (defined(__G1) + defined(__G1A) + defined(__G2) + defined(__G2A)) > 1
+			return true;
+#else
+			return false;
+#endif
+		}
+
+		template<typename T, typename... Args>
+		struct FunctionTraitsBase
+		{
+			using ReturnType = T;
+			using ArgTypes = std::tuple<Args...>;
+			static constexpr std::size_t ArgCount = sizeof...(Args);
+			static constexpr bool HasReturn = !std::is_void_v<ReturnType>;
+		};
+
+		template<typename T> struct FunctionTraits;
+
+		template<typename R, typename... Args>
+		struct FunctionTraits<R(*)(Args...)>
+			: FunctionTraitsBase<R, Args...>
+		{
+			using Pointer = R(*)(Args...);
+			using ArgTypes = std::tuple<Args...>;
+		};
+
+		template<typename R>
+		struct FunctionTraits<R(*)()>
+			: FunctionTraitsBase<R>
+		{
+			using Pointer = R(*)();
+		};
+
+		template<std::size_t N>
+		struct FixedStr
+		{
+			static inline constexpr char CharToUpperSimple(const char t_char)
+			{
+				return t_char >= 'a' && t_char <= 'z'
+					? static_cast<char>(static_cast<unsigned char>(t_char) - ('a' + 'A'))
+					: t_char;
+			}
+
+			static inline constexpr char CharToLowerSimple(const char t_char)
+			{
+				return t_char >= 'A' && t_char <= 'Z'
+					? static_cast<char>(static_cast<unsigned char>(t_char) + ('a' - 'A'))
+					: t_char;
+			}
+
+			constexpr FixedStr(const char(&source)[N + 1])
+			{
+				std::copy(std::cbegin(source), std::cend(source), begin());
+			}
+
+			template<std::size_t LeftSize, std::size_t RightSize>
+			constexpr FixedStr(const FixedStr<LeftSize>& t_left, const FixedStr<RightSize>& t_right)
+			{
+				static_assert(LeftSize + RightSize == Size);
+
+				std::copy(t_left.cbegin(), t_left.cend(), begin());
+				std::copy(t_right.cbegin(), t_right.cend(), std::next(begin(), static_cast<std::ptrdiff_t>(t_left.size())));
+			}
+
+			constexpr auto& Upper()
+			{
+				std::ranges::transform(m_array, std::begin(m_array), CharToUpperSimple);
+				return *this;
+			}
+
+			constexpr auto& Lower()
+			{
+				std::ranges::transform(m_array, std::begin(m_array), CharToLowerSimple);
+				return *this;
+			}
+
+			[[nodiscard]]
+			constexpr size_t Size() const noexcept
+			{
+				return Size;
+			}
+
+			[[nodiscard]]
+			constexpr char* begin()
+			{
+				return m_array.data();
+			}
+
+			[[nodiscard]]
+			constexpr char* end()
+			{
+				return std::next(m_array.data(), Size());
+			}
+
+			[[nodiscard]]
+			constexpr const char* cbegin() const
+			{
+				return m_array.data();
+			}
+
+			[[nodiscard]]
+			constexpr const char* cend() const
+			{
+				return std::next(m_array.data(), static_cast<std::ptrdiff_t>(Size()));
+			}
+
+			[[nodiscard]]
+			constexpr std::string_view Data() const noexcept
+			{
+				return std::string_view{ m_array.data(), m_array.size() };
+			}
+
+			template<std::size_t LeftSize, std::size_t RightSize>
+			friend constexpr auto operator+(const FixedStr<LeftSize>& t_left, const FixedStr<RightSize>& t_right);
+
+			std::array<char, N + 1> m_array{};
+		};
+
+		template<std::size_t Size>
+		FixedStr(const char(&)[Size]) -> FixedStr<Size - 1>;
+
+		template<std::size_t LeftSize, std::size_t RightSize>
+		FixedStr(FixedStr<LeftSize> t_left, FixedStr<RightSize> t_right) -> FixedStr<LeftSize + RightSize>;
+
+		template<std::size_t LeftSize, std::size_t RightSize>
+		constexpr auto operator+(const FixedStr<LeftSize>& t_left, const FixedStr<RightSize>& t_right)
+		{
+			return FixedStr(t_left, t_right);
+		};
+
+
+		template<class Value, typename Key,
+			typename Container = std::vector<Value>>
+			struct MappedInstance
+		{
+			using KeyType = Key;
+			using ValueType = Value;
+			using MappedBase = MappedInstance<Value, Key, Container>;
+
+			constexpr MappedInstance(const Key& t_key)
+				: m_key(t_key)
+			{
+			}
+
+			//TODO use static constexpr Value& operator[] if MSVC will get this implemented
+			[[nodiscard]] static Value& Get(const Key& t_key)
+			{
+#define Unlikely [[unlikely]]
+#define Likely	[[unlikely]]
+
+				const auto SameKey = [&t_key](const auto& t_object)
+					{
+						return t_object.m_key == t_key;
+					};
+
+				auto& value = [&]() -> Value&
+					{
+						if (const auto it = std::ranges::find_if(s_objects, SameKey);
+							it != End()) Likely
+						{
+							return *it;
+						}
+						else Unlikely
+						{
+							return s_objects.emplace_back(Value{t_key});
+						}
+
+					}();
+#undef Unlikely
+#undef Likely
+					return value;
+			}
+
+			static void ForEach(auto&& t_callable)
+			{
+				std::ranges::for_each(s_objects, t_callable);
+			}
+
+			static typename Container::iterator FindIf(auto&& t_callable)
+			{
+				return std::ranges::find_if(s_objects, t_callable);
+			}
+
+			static typename Container::iterator Find(const Key& t_key)
+			{
+				const auto SameKey = [&t_key](const auto& t_value)
+					{
+						return t_key == t_value.m_key;
+					};
+
+				return std::ranges::find_if(s_objects, SameKey);
+			}
+
+			static auto Begin()
+			{
+				return std::begin(s_objects);
+			}
+
+			static auto End()
+			{
+				return std::end(s_objects);
+			}
+
+			static void Clear()
+			{
+				s_objects.clear();
+			}
+
+			Key m_key;
+
+
+			inline static Container s_objects;
+		};
+
+		
 		struct DaedalusFunction 
 		{
 			int m_index{ -1 };
@@ -180,7 +393,7 @@ namespace GOTHIC_ENGINE
 
 		struct BaseExternal
 		{
-			static void DefineExternal([[maybe_unused]] zCParser* const t_parser) {}
+			virtual void DefineExternal([[maybe_unused]] zCParser* const t_parser) const {}
 
 			inline static zSTRING* nameBuffer = []()
 				{
@@ -195,7 +408,7 @@ namespace GOTHIC_ENGINE
 					return new zSTRING;
 
 				}();
-
+			
 		};
 
 
@@ -259,7 +472,7 @@ namespace GOTHIC_ENGINE
 					return 0;
 			}
 
-			static void DefineExternal(zCParser* const t_parser)
+			virtual void DefineExternal(zCParser* const t_parser) const
 			{
 				if (!Condition())
 				{
@@ -283,42 +496,78 @@ namespace GOTHIC_ENGINE
 				}
 			}
 		};
-
-		template<typename T1, typename T2>
-		constexpr bool ExternalSameAs()
-		{
-			return T1::s_name.Data() == T2::s_name.Data();
-		}
-
-		template<typename T, typename... Types>
-		constexpr bool are_externals_unique_v = ((!ExternalSameAs<T, Types>()) && ...) && are_externals_unique_v<Types...>;
-
-		template<typename T>
-		constexpr bool are_externals_unique_v<T> = true;
-
-		template <typename T, typename... Ts>
-		constexpr bool are_base_of_v = (std::is_base_of_v<T, Ts> && ...);
-
-		template<typename...Args>
-			requires (are_externals_unique_v<Args...> && are_base_of_v<BaseExternal,Args...>)
-		using ExternalsTuple = std::tuple<Args...>;
-
-		template<eParser Parser>
+	
 		struct ExternalTable
 		{
-			static constexpr const auto s_parser = Parser;
+			ExternalTable(const eParser t_parserEnum, std::vector<BaseExternal*> t_externals)
+				:m_parser(GetParserByEnum(t_parserEnum)),
+				m_externals(std::move(t_externals))
+				
+			{
+				s_tables.push_back(this);
+			}
 
-			using Table = ExternalsTuple
-				<
-				BaseExternal
-				>;
+			void Define() const
+			{
+				for (const auto& ext : m_externals)
+				{
+					ext->DefineExternal(m_parser);
+				}
 
+			}
+
+			static void DefineAll()
+			{
+				for (const auto table : s_tables)
+				{
+					table->Define();
+				}
+			}
+
+			~ExternalTable()
+			{
+				for (auto ptr : m_externals)
+				{
+					delete ptr;
+				}
+
+				m_externals.clear();
+			}
+
+			std::vector<BaseExternal*> m_externals;
+			zCParser* m_parser;
+
+			inline static std::vector<ExternalTable*> s_tables;
 		};
 
-		template<size_t N> 
-			requires(N < std::to_underlying(eParser::MAX))
-		using NthTable = ExternalTable<static_cast<eParser>(N)>;
 
+		template<eParser Parser>
+		struct ExternalTableGuard
+		{
+		};
 
+		void __fastcall zCPar_DataStack__Clear(zCPar_DataStack* t_this, void* t_reg);
+		inline HOOK Hook_zCPar_DataStack__Clear PATCH(&zCPar_DataStack::Clear, &zCPar_DataStack__Clear);
+
+		void __fastcall oCGame__DefineExternals_Ulfi(oCGame* t_this, void* t_reg, zCParser* t_parser);
+		inline HOOK Hook_oCGame__DefineExternals_Ulfi PATCH(oCGame::DefineExternals_Ulfi, &oCGame__DefineExternals_Ulfi);
+
+		void DefineExternals()
+		{
+			ExternalTable::DefineAll();
+		}
+
+		void __fastcall oCGame__DefineExternals_Ulfi(oCGame* t_this, void* t_reg, zCParser* t_parser)
+		{
+			Hook_oCGame__DefineExternals_Ulfi(t_this, t_reg, t_parser);
+			DefineExternals();
+
+		}
+		void __fastcall zCPar_DataStack__Clear(zCPar_DataStack* t_this, void* t_reg)
+		{
+			Hook_zCPar_DataStack__Clear(t_this, t_reg);
+			Externals::StringPool::ClearPool(t_this);
+		}
 	}
+
 }
